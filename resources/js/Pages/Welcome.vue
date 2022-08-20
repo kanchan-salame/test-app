9db0cfb9a8f4d6530296e38e67782540f5e666cd
kj173742 <48567466+kj173742@users.noreply.github.com>
2022-08-20 11:27:05 +0530

    Sidebar and Welcome message changes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -19,7 +19,7 @@ defineProps({
             <template v-else>
                 <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
+                <!-- <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link> -->
             </template>
         </div>
 
